6016ac033d4f9d7caf0659a168a18eea48b388ca
Bartpbcn <162989852+Bartpbcn@users.noreply.github.com>
2024-03-12 11:28:25 +0100

    review changes

--- hello_world.php
+++ hello_world.php
@@ -1,5 +1,5 @@
 <?php
-echo "hello world." . PHP_EOL . "I use br to have a break."; PHP_EOL;
+echo "hello world." . PHP_EOL . "I use br to have a break." .  PHP_EOL;
 echo "to use the same language that is understandable by html and PHP i use php eol instead.";
 echo "If not everything will be on 1 line" . PHP_EOL;
 

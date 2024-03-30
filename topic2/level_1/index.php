8076673bb6c575cb2b4feca6e59e3bc2ff37d2fc
Bartpbcn <162989852+Bartpbcn@users.noreply.github.com>
2024-03-30 11:34:43 +0100

    exercise1

---
+++ topic2/level_1/index.php
@@ -0,0 +1,17 @@
+<?php
+
+$integerVar = 12;
+$doubleVar = 0.12;
+$stringVar = "Hello";
+$booleanVar = "true";
+
+echo "Integer:" . $integerVar . "<br>";
+echo "Double:" . $doubleVar . "<br>";
+echo "String:" . $stringVar . "<br>";
+echo "Boolean:" . $booleanVar . "<br>";
+
+define("MY_NAME", "Bart");
+
+echo "<title>" . MY_NAME . "</title>";
+
+?>

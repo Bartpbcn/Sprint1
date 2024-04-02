892ab183d513e943bd5d88857f594ff990cd9e7f
Bartpbcn <162989852+Bartpbcn@users.noreply.github.com>
2024-04-02 14:02:36 +0200

    defince the marks for each student

--- topic3/level2/ecercise_2.php
+++ topic3/level2/ecercise_2.php
@@ -1,5 +1,32 @@
 <?php
+$test_Mathematics = array(
+    "Bart" => 8,
+    "Monica" => 9,
+    "Hubert" => 5,
+);
 
+$test_Informatics = array(
+    "Bart" => 8,
+    "Monica" => 9.5,
+    "Hubert" => 4,
+);
 
+$test_Spanish = array(
+    "Bart" => 7,
+    "Monica" => 10,
+    "Hubert" => 6,
+);
+
+$test_Dutch = array(
+    "Bart" => 10,
+    "Monica" => 2,
+    "Hubert" => 4,
+);
+
+$test_Bio = array(
+    "Bart" => 9,
+    "Monica" => 6,
+    "Hubert" => 5,
+);
 
 ?>

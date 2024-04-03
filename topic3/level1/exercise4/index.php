b511c4547d5e924f8977f2989a06b226fccb8fe4
Bartpbcn <162989852+Bartpbcn@users.noreply.github.com>
2024-04-03 11:43:43 +0200

    personal info array initiated

---
+++ topic3/level1/exercise4/index.php
@@ -0,0 +1,20 @@
+<?php
+/*Make an associative array that represents information about yourself. Specifically, it must include:
+
+first name
+age
+email
+favorite food
+*/
+
+
+$PersonalInfo = [
+
+    "firstName" => "Bart",
+    "age" => "38",
+    "email" => "bartpauwels@me.com"
+    "food" => "steak with Bearnaise and Belgian fries"
+
+];
+
+?>

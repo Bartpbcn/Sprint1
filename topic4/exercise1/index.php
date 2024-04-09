7fff1eb617d142cc0e219d9addace1577a28269d
Bartpbcn <162989852+Bartpbcn@users.noreply.github.com>
2024-04-09 12:36:15 +0200

    create attrubutes and contruct method

---
+++ topic4/exercise1/index.php
@@ -0,0 +1,25 @@
+<?php
+class Employee {
+//Atributes
+    public $name = "";
+    public $salary = 0;
+
+///method construct
+public function __construct($name, $salary){ // opzetten in inizialiseren van het object en parameters
+ $this-> name;
+ $this-> salary;
+}
+public function print (){
+    echo $this-> name;
+    echo $this-> salary;
+ }
+
+
+
+    
+
+
+}
+
+
+?>

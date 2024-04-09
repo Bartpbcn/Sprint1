451c5720a53001e9f560148fa7b685a65643787e
Bartpbcn <162989852+Bartpbcn@users.noreply.github.com>
2024-04-09 13:41:29 +0200

    name didnt print, there was a problem at the construct, deleted the call of the construct-already in creating object

--- topic4/exercise1/index.php
+++ topic4/exercise1/index.php
@@ -6,22 +6,24 @@ class Employee {
 
 ///method construct
 public function __construct($name, $salary){ // opzetten in inizialiseren van het object en parameters
- $this-> name = $name;
- $this-> salary = $salary;
+    
+    $this->name = $name;
+  $this->salary = $salary;
 }
 public function printTaxInformation(){
     if ($this->salary > 6000){
-        echo $this-> name . ", you have to pay taxes.";    
+        echo $this->name . ", you have to pay taxes.";    
     }
     else {
-        echo $this-> name . ", you dont have to pay taxes.";     
+        echo $this->name . ", you dont have to pay taxes.";     
     }
    }
-
+}
 
 //creation object
-$employee= new Employee("Bart", 7000);
+$employee = new Employee("Bart", 7000);
 //call or envoke the method
+
 $employee-> printTaxInformation();
 
 

3af8417141f1e751a1e4900d93e5d89dc11a71c0
Bartpbcn <162989852+Bartpbcn@users.noreply.github.com>
2024-04-09 13:17:02 +0200

    creation object and call method

--- topic4/exercise1/index.php
+++ topic4/exercise1/index.php
@@ -1,25 +1,33 @@
 <?php
 class Employee {
 //Atributes
-    public $name = "";
-    public $salary = 0;
+    public $name;
+    public $salary;
 
 ///method construct
 public function __construct($name, $salary){ // opzetten in inizialiseren van het object en parameters
- $this-> name;
- $this-> salary;
+ $this-> name = $name;
+ $this-> salary = $salary;
 }
-public function print (){
-    echo $this-> name;
-    echo $this-> salary;
- }
+public function printTaxInformation(){
+    if ($this->salary > 6000){
+        echo $this-> name . ", you have to pay taxes.";    
+    }
+    else {
+        echo $this-> name . ", you dont have to pay taxes.";     
+    }
+   }
+
+
+//creation object
+$employee= new Employee("Bart", 7000);
+//call or envoke the method
+$employee-> printTaxInformation();
+
 
 
 
     
-
-
-}
 
 
 ?>

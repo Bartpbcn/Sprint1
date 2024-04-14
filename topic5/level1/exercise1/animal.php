06666cf9e35bd73cefdb14e1e0a353efb7a02cd2
Bartpbcn <162989852+Bartpbcn@users.noreply.github.com>
2024-04-15 01:34:36 +0200

    object creation and call method

--- topic5/level1/exercise1/animal.php
+++ topic5/level1/exercise1/animal.php
@@ -14,3 +14,14 @@ abstract class Animal{
 abstract public function makeSound();
 
 }
+
+//object creation and store in the var
+$dog = new Dog("Buddy");
+$cat = new Cat ("Shadow");
+
+// call the nethod for each object
+echo $dog->makeSound() . PHP_EOL;
+echo $cat->makeSound() . PHP_EOL; 
+
+
+?>

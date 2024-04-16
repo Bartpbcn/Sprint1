9c07a090b42f87b42a61f13c3be0ef4d3b20d351
Bartpbcn <162989852+Bartpbcn@users.noreply.github.com>
2024-04-16 11:45:54 +0200

    deleted unnessary code and commend info for ourselves on server side

--- topic6/exercise1/formhandler.php
+++ topic6/exercise1/formhandler.php
@@ -1,27 +1,26 @@
 <?php
 
-//to avoid that
 
 
-//check this every time
+//run this every time for every fill in
 if($_SERVER["REQUEST_METHOD"]== "POST"){
 
-    $firstname = htmlspecialchars($_POST["firstname"]); //htmlspsecialchar noone can insert code in the form to hack your code. like this the server takes it like htnml entities
+    $firstname = htmlspecialchars($_POST["firstname"]); //htmlspsecialchar is an option so no one can insert code in the form to hack your code. like this the server takes it like htnml entities
     $lastname = htmlspecialchars($_POST["lastname"]);
     $username = htmlspecialchars($_POST["username"]);
     $email = htmlspecialchars($_POST["email"]);
-
-echo "These are the data that the user submitted:";
-echo "<br>";
-echo $firstname
-echo "<br>";
-echo $lastname
-echo "<br>";
-echo $username
-echo "<br>";
-echo $email
 }
+    
+//info for ourselves when user submitted:
+/*echo "These are the data that the user submitted:";
+echo "<br>";
+echo $firstname . "<br>";
 
+echo $lastname . "<br>";
 
+echo $username . "<br>";
 
+echo $email . "<br>";
+
+*/
 ?>
